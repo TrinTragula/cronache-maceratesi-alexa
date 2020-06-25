a2be9fb8e93eaf5406663c4117079da543570b6f
TrinTragula <danielescarinci42@gmail.com>
2020-06-25 19:41:56 +0200

    Support for more news after prompt

--- lambda/cronache_maceratesi.py
+++ lambda/cronache_maceratesi.py
@@ -5,14 +5,18 @@ RSS_URL = "https://www.cronachemaceratesi.it/feed/"
 MAX_ARTICLES_TO_READ = 5
 
 
-def get_latest_news(category=None):
+def get_latest_news(current_page, category=None):
     try:
         feed = feedparser.parse(RSS_URL)
         news = feed.entries
-
+        current_page = int(current_page)
+        start_item = current_page * MAX_ARTICLES_TO_READ
+        end_item = MAX_ARTICLES_TO_READ + current_page * MAX_ARTICLES_TO_READ
         if (news is not None and len(news) >= 0):
+            news = [n for n in news if "&#" not in n["summary"]]
+
             if category is None:
-                return [get_clean_string_from_news(i) for i in news[:MAX_ARTICLES_TO_READ]]
+                return get_internal(news, start_item, end_item)
             else:
                 filtered_news = []
                 for n in news:
@@ -20,18 +24,26 @@ def get_latest_news(category=None):
                         if category.strip().lower() in re.sub(r"[^\w\s]", "", tag["term"].strip().lower()):
                             filtered_news.append(n)
                             break
-                return [get_clean_string_from_news(i) for i in filtered_news[:MAX_ARTICLES_TO_READ]]
+                return get_internal(filtered_news, start_item, end_item)
         else:
             return []
     except:
         return []
 
 
+def get_internal(news, start_item, end_item):
+    if start_item > len(news):
+        return []
+    if end_item > len(news):
+        end_item = len(news)
+    return [get_clean_string_from_news(i) for i in news[start_item:end_item]]
+
+
 def get_clean_string_from_news(news):
     author = news["author"]
     splitted_summary = news["summary"].split(" - ")
     argument = splitted_summary[0]
-    real_summary = splitted_summary[1]
+    real_summary = "" if len(splitted_summary) == 1 else splitted_summary[1]
 
     utterance = "{0}, di {1}. {2}. {3}. ".format(
         argument, author, news["title"], real_summary
@@ -40,15 +52,20 @@ def get_clean_string_from_news(news):
     for char in ["«", "»", "“", "”"]:
         utterance = utterance.replace(char, "\"")
     utterance = utterance.replace("(Foto/Video)", "")
+    utterance = utterance.replace("A'", "à")
+    utterance = utterance.replace("E'", "è")
+    utterance = utterance.replace("I'", "ì")
+    utterance = utterance.replace("O'", "ò")
+    utterance = utterance.replace("U'", "ù")
     utterance = re.sub(r"\s+", " ", utterance)
     utterance = re.sub(r"(\s\d\d)\.(\d\d\s)", r"\1:\2", utterance)
 
     return utterance.strip()
 
 
-# for n in get_latest_news("civitanova"):
-#     print(n)
-#     print("===")
+for n in get_latest_news(0, "camerino"):
+    print(n)
+    print("===")
 
 
 # feed = feedparser.parse('https://www.cronachemaceratesi.it/feed/')
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -15,14 +15,49 @@ from cronache_maceratesi import get_latest_news
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+NEWS_PAGE_KEY = "NEWS_PAGE_NUMBER"
+CATEGORY_KEY = "NEWS_CATEGORY"
+
 SENTENCES = {
     "launch": "Ciao, chiedimi di dirti le ultime notizie!",
+    "invite_to_ask_more": "Chiedimi di nuovo le ultime notizie o notizie su di un particolare argomento.",
     "no_news": "Non ho trovato nuove notizie.",
     "no_news_on_category": "Non ho trovato notizie relative a questo argomento.",
+    "no_more_news": "Non ho trovato altre notizie.",
     "help": "Chiedimi le ultime notizie e ti leggerò gli ultimi articoli pubblicati da Cronache Maceratesi. Puoi anche chiedere le ultime notizie per la tua città. Prova a chiedere le ultime notizie su 'Camerino'",
     "stop": "A presto!",
-    "exception": "Scusa, non ho capito, puoi ripetere?"
+    "exception": "Scusa, non ho capito, puoi ripetere?",
+    "more": "Vuoi che legga altre notizie?",
+    "pause": "<break time='1s'/> "
 }
+
+
+def get_current_page_from_session(handler_input):
+    return 0 if NEWS_PAGE_KEY not in handler_input.attributes_manager.session_attributes else handler_input.attributes_manager.session_attributes[
+        NEWS_PAGE_KEY]
+
+
+def get_category_from_session(handler_input):
+    return None if CATEGORY_KEY not in handler_input.attributes_manager.session_attributes else handler_input.attributes_manager.session_attributes[
+        CATEGORY_KEY]
+
+
+def get_answer(news, category=None, is_more=False):
+    if is_more:
+        incipit = ""
+    else:
+        incipit = "Ecco le ultime notizie"
+        if category is not None:
+            incipit += "su {}".format(category)
+
+    news_string = SENTENCES["pause"].join(news)
+
+    return "{}. {}  <break time='1s'/> {}".format(incipit, news_string, SENTENCES["more"])
+
+
+def reset_all(handler_input):
+    handler_input.attributes_manager.session_attributes[NEWS_PAGE_KEY] = 0
+    handler_input.attributes_manager.session_attributes[CATEGORY_KEY] = None
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
@@ -35,6 +70,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        reset_all(handler_input)
         speak_output = SENTENCES["launch"]
 
         return (
@@ -54,19 +90,29 @@ class LatestNewsIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        news = get_latest_news()
-        speak_output = "Ecco le ultime notizie. " + " ".join(news)
+        reset_all(handler_input)
+
+        current_page = 0
+
+        news = get_latest_news(current_page)
+        speak_output = get_answer(news)
 
         if news is None or news == []:
+            reset_all(handler_input)
             return (
                 handler_input.response_builder
-                .speak(SENTENCES["no_news"])
+                .speak(SENTENCES["no_news"] + " " + SENTENCES["invite_to_ask_more"])
+                .ask(SENTENCES["invite_to_ask_more"])
                 .response
             )
+
+        handler_input.attributes_manager.session_attributes[NEWS_PAGE_KEY] = current_page + 1
+        handler_input.attributes_manager.session_attributes[CATEGORY_KEY] = None
 
         return (
             handler_input.response_builder
             .speak(speak_output)
+            .ask(SENTENCES["more"])
             .response
         )
 
@@ -80,26 +126,70 @@ class LatestNewsOnCategoryIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        reset_all(handler_input)
+
+        current_page = 0
 
         slots = handler_input.request_envelope.request.intent.slots
         category = slots["Category"]
         if category.value:
-            news = get_latest_news(category.value)
-            speak_output = "Ecco le ultime notizie su " + \
-                category.value + ". " + " ".join(news)
+            news = get_latest_news(current_page, category.value)
+            speak_output = get_answer(news, category.value)
         else:
             news = []
 
         if news is None or news == []:
+            reset_all(handler_input)
             return (
                 handler_input.response_builder
-                .speak(SENTENCES["no_news_on_category"])
+                .speak(SENTENCES["no_news_on_category"] + " " + SENTENCES["invite_to_ask_more"])
+                .ask(SENTENCES["invite_to_ask_more"])
+                .response
+            )
+
+        handler_input.attributes_manager.session_attributes[NEWS_PAGE_KEY] = current_page + 1
+        handler_input.attributes_manager.session_attributes[CATEGORY_KEY] = category.value
+
+        return (
+            handler_input.response_builder
+            .speak(speak_output)
+            .ask(SENTENCES["more"])
+            .response
+        )
+
+
+class YesIntentHandler(AbstractRequestHandler):
+    """Handler for Yes Intent."""
+
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.YesIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        current_page = get_current_page_from_session(handler_input)
+        category = get_category_from_session(handler_input)
+
+        news = get_latest_news(current_page, category)
+        speak_output = get_answer(news, category)
+
+        handler_input.attributes_manager.session_attributes[NEWS_PAGE_KEY] = current_page + 1
+        handler_input.attributes_manager.session_attributes[CATEGORY_KEY] = category
+
+        if news is None or news == []:
+            reset_all(handler_input)
+            return (
+                handler_input.response_builder
+                .speak(SENTENCES["no_more_news"] + " " + SENTENCES["invite_to_ask_more"])
+                .ask(SENTENCES["invite_to_ask_more"])
                 .response
             )
 
         return (
             handler_input.response_builder
             .speak(speak_output)
+            .ask(SENTENCES["more"])
             .response
         )
 
@@ -129,10 +219,13 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.NoIntent")(handler_input))
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
+        reset_all(handler_input)
+
         speak_output = SENTENCES["stop"]
 
         return (
@@ -153,7 +246,7 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
         # type: (HandlerInput) -> Response
 
         # Any cleanup logic goes here.
-        # Empty for now
+        reset_all(handler_input)
 
         return handler_input.response_builder.response
 
@@ -213,6 +306,7 @@ sb.add_request_handler(LatestNewsOnCategoryIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(YesIntentHandler())
 
 # IntentReflectorHandler must be the last before the exception handler
 sb.add_request_handler(IntentReflectorHandler())
